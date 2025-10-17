b7f3b2c6a155fde41f8bc355463e43dccbb511ea
Meagan Enguerra <meagansilvano@gmail.com>
2025-10-17 22:05:47 +0800

    1dwa

--- trylang/Program.cs
+++ trylang/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!, try lang po, TRY 1, 2, 3, 4, row , row 1; try ulit");
+Console.WriteLine("Hello, World!, try lang po, TRY 1, 2, 3, 4, row , row 1; try ulit, ahh");
